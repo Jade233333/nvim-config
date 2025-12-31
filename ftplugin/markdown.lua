d0e294ec2ea6b668de62df9c36a598a54a898215
zhou_arch <hatesjade@gmail.com>
2025-12-31 09:35:30 +0800

    cursor and markdown

---
+++ ftplugin/markdown.lua
@@ -0,0 +1,3 @@
+vim.opt_local.foldmethod = "expr"
+vim.opt_local.foldexpr = "MarkdownFold()"
+vim.opt_local.foldlevel = 99
--- lua/jade/remap.lua
+++ lua/jade/remap.lua
@@ -7,7 +7,10 @@ vim.keymap.set("n","<leader>ls",vim.cmd.Ex)
 vim.keymap.set("v","J",":m '>+1<CR>gv=gv")
 vim.keymap.set("v","K",":m '<-2<CR>gv=gv")
 
+-- Move by visual line when lines wrap.
+vim.keymap.set({ "n", "x" }, "j", "gj")
+vim.keymap.set({ "n", "x" }, "k", "gk")
+
 vim.keymap.set("n", "<leader>f", function()
   vim.lsp.buf.format({ async = true })
 end)
-
