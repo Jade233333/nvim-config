5dad6278b59993efb449cc29cc1831b9d39bdc7f
zhou_arch <hatesjade@gmail.com>
2025-12-31 12:05:06 +0800

    update

--- lua/jade/remap.lua
+++ lua/jade/remap.lua
@@ -2,15 +2,20 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- ls for directory listing
-vim.keymap.set("n","<leader>ls",vim.cmd.Ex)
+vim.keymap.set("n", "<leader>ls", vim.cmd.Ex)
 
-vim.keymap.set("v","J",":m '>+1<CR>gv=gv")
-vim.keymap.set("v","K",":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- Move by visual line when lines wrap.
 vim.keymap.set({ "n", "x" }, "j", "gj")
 vim.keymap.set({ "n", "x" }, "k", "gk")
 
-vim.keymap.set("n", "<leader>f", function()
-  vim.lsp.buf.format({ async = true })
+vim.keymap.set("n", "<leader>fm", function()
+    vim.lsp.buf.format({ async = true })
 end)
+
+vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], {
+    noremap =
+        true
+})
