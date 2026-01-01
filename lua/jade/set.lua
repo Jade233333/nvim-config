ffc2c04f82c9ca501ba2ce79ffb43fc527e20d1c
zhou_arch <hatesjade@gmail.com>
2026-01-01 08:40:45 +0800

    remove status line

--- lua/jade/set.lua
+++ lua/jade/set.lua
@@ -1,35 +1,36 @@
-vim.opt.nu = true
-vim.opt.relativenumber = true
+vim.o.nu = true
+vim.o.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
 
-vim.opt.smartindent = true
+vim.o.smartindent = true
 
-vim.opt.wrap = false
-vim.opt.linebreak = true
-vim.opt.breakindent = true
+vim.o.wrap = false
+vim.o.linebreak = true
+vim.o.breakindent = true
 
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
+vim.o.swapfile = false
+vim.o.backup = false
+vim.o.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.o.undofile = true
 
-vim.opt.hlsearch = true
-vim.opt.incsearch = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+vim.o.hlsearch = true
+vim.o.incsearch = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
 
-vim.opt.termguicolors = true
+vim.o.termguicolors = true
 
-vim.opt.scrolloff = 10
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
+vim.o.scrolloff = 10
+vim.o.signcolumn = "yes"
+vim.o.isfname:append("@-@")
 
-vim.opt.updatetime = 50
+vim.o.updatetime = 50
 
 vim.g.mapleader = " "
 
-vim.opt.clipboard = "unnamedplus"
+vim.o.clipboard = "unnamedplus"
+vim.o.laststatus = 0
