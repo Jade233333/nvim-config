bd7443e9177251f6637f33019f717170a4b5c0b7
zhou_air <hatesjade@gmail.com>
2026-01-23 14:09:29 -0600

    moving on to gruvbox

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,10 +1,11 @@
-return{ "catppuccin/nvim", 
-	name = "catppuccin", 
-	priority = 1000,
-	opts = {
-      flavour = "mocha", -- latte, frappe, macchiato, mocha
-    },
-	config = function()
-		vim.cmd('colorscheme catppuccin')
-	end
-	}
+return {}
+-- return{ "catppuccin/nvim", 
+-- 	name = "catppuccin", 
+-- 	priority = 1000,
+-- 	opts = {
+--       flavour = "mocha", -- latte, frappe, macchiato, mocha
+--     },
+-- 	config = function()
+-- 		vim.cmd('colorscheme catppuccin')
+-- 	end
+-- 	}
---
+++ lua/plugins/gruvbox.lua
@@ -0,0 +1,14 @@
+return {
+  "ellisonleao/gruvbox.nvim",
+  priority = 1000,
+  config = function()
+    vim.o.termguicolors = true
+    vim.o.background = "dark"
+    require("gruvbox").setup({
+      contrast = "hard", -- key line
+      transparent_mode = true, -- optional (matches terminal bg)
+    })
+    vim.cmd.colorscheme("gruvbox")
+  end,
+}
+
