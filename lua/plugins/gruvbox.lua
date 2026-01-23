c46a8dc1fb70699514129f8544a02257a92b787c
zhou_air <hatesjade@gmail.com>
2026-01-23 16:36:10 -0600

    gruvbox disable hard

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -5,8 +5,8 @@ return {
     vim.o.termguicolors = true
     vim.o.background = "dark"
     require("gruvbox").setup({
-      contrast = "hard", -- key line
-      transparent_mode = true, -- optional (matches terminal bg)
+      -- contrast = "hard", -- key line
+      -- transparent_mode = true, -- optional (matches terminal bg)
     })
     vim.cmd.colorscheme("gruvbox")
   end,
