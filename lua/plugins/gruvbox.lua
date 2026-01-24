ee335cd458c7eb159e6179e11545c594100e1c2e
zhou_arch <hatesjade@gmail.com>
2026-01-23 19:30:14 -0600

    init

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -1,14 +1,15 @@
-return {
-  "ellisonleao/gruvbox.nvim",
-  priority = 1000,
-  config = function()
-    vim.o.termguicolors = true
-    vim.o.background = "dark"
-    require("gruvbox").setup({
-      -- contrast = "hard", -- key line
-      -- transparent_mode = true, -- optional (matches terminal bg)
-    })
-    vim.cmd.colorscheme("gruvbox")
-  end,
-}
+return{}
+-- return {
+--   "ellisonleao/gruvbox.nvim",
+--   priority = 1000,
+--   config = function()
+--     vim.o.termguicolors = true
+--     vim.o.background = "dark"
+--     require("gruvbox").setup({
+--       -- contrast = "hard", -- key line
+--       -- transparent_mode = true, -- optional (matches terminal bg)
+--     })
+--     vim.cmd.colorscheme("gruvbox")
+--   end,
+-- }
 
