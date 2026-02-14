8b7fac034cecbc94ba54ba0a110e9335907d3c72
zhou_air <hatesjade@gmail.com>
2026-02-14 12:16:26 -0600

    local open

---
+++ lua/jade/localopen.lua
@@ -0,0 +1,8 @@
+local function open_current_file()
+  local file = vim.fn.fnameescape(vim.fn.expand('%'))
+  vim.cmd('!open ' .. file)
+end
+
+vim.keymap.set('n', '<leader><CR>', open_current_file, {
+  desc = 'Open current file',
+})
