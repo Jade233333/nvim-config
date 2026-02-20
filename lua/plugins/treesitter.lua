284e5078d1a8526b92508f6a6912140e4100c11d
zhou_air <hatesjade@gmail.com>
2026-02-20 17:18:43 -0600

    ts

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,4 +2,21 @@ return {
     'nvim-treesitter/nvim-treesitter',
     lazy = false,
     build = ':TSUpdate',
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = {
+                "markdown",
+                "markdown_inline",
+                "lua",
+                "python",
+                "c",
+                "typst",
+                "latex",
+                "bash"
+            },
+            highlight = {
+                enable = true,
+            },
+        })
+    end,
 }
