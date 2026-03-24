cea9961f46c55f3c1d1e5c2a3ec908a074c2f607
zhou_air <hatesjade@gmail.com>
2026-03-24 07:04:20 -0500

    riscv config

--- lua/plugins/riscv.lua
+++ lua/plugins/riscv.lua
@@ -1,12 +1,13 @@
 return {
-  "henry-hsieh/riscv-asm-vim",
-  ft = { "riscv_asm" },
-  init = function()
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = "riscv_asm",
-      callback = function()
-        vim.bo.commentstring = "# %s"
-      end,
-    })
-  end,
+    "henry-hsieh/riscv-asm-vim",
+    ft = { "riscv_asm" },
+    init = function()
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = "riscv_asm",
+            callback = function()
+                vim.g.riscv_asm_all_enable = true
+                -- vim.bo.commentstring = "# %s"
+            end,
+        })
+    end,
 }
