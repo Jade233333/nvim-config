a6278974d502d40898b387dc9b40debe89a20ec0
zhou_air <hatesjade@gmail.com>
2026-04-18 00:47:46 -0500

    config

--- lua/jade/treesitter.lua
+++ lua/jade/treesitter.lua
@@ -1,3 +1,22 @@
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt.foldlevel = 99
+
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = { 
+        'lua',
+        'bash',
+        'sh',
+        'python',
+        'markdown',
+        'c',
+        'cpp',
+        'typst',
+        'yaml',
+        'html',
+        'css',
+        'javascript',
+        'typescript',
+    },
+    callback = function() vim.treesitter.start() end,
+})
