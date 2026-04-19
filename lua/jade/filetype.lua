07c9f97f53fff1918acecab6226cce508b29215d
zhou_air <hatesjade@gmail.com>
2026-04-18 19:53:57 -0500

    something

--- lua/jade/filetype.lua
+++ lua/jade/filetype.lua
@@ -1,3 +1,9 @@
+vim.filetype.add({
+    extension = {
+        mustache = 'html',
+    },
+})
+
 -- Enable wrapping for typst and markdown files
 vim.api.nvim_create_autocmd("FileType", {
     pattern = { "typst", "markdown" },
