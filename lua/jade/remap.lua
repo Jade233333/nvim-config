931f0a46ab334e6b3cafab992b951f90b7de11a2
zhou_air <hatesjade@gmail.com>
2026-04-20 11:54:05 -0500

    quick fix

--- lua/jade/remap.lua
+++ lua/jade/remap.lua
@@ -1,3 +1,5 @@
+-- remap.lua
+-- this file stores the remap related to vim native commands
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
@@ -10,6 +12,12 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set({ "n", "x" }, "j", "gj")
 vim.keymap.set({ "n", "x" }, "k", "gk")
 
+-- terminal remap
 vim.keymap.set("t", "<C-q>", [[<C-\><C-n>]])
---
--- vim.keymap.set("n", "<leader>s", ":term<CR>")
+
+-- quick fix remap
+-- c for "cuick"
+vim.keymap.set('n', '<leader>co', ':copen<CR>')
+vim.keymap.set('n', '<leader>cc', ':cclose<CR>')
+vim.keymap.set('n', '<leader>cn', ':cnext<CR>')
+vim.keymap.set('n', '<leader>cp', ':cprev<CR>')
