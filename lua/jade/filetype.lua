c63ad3c13af19d2a6cc7537eeb08a238270146fc
zhou_air <hatesjade@gmail.com>
2026-04-23 02:03:31 -0500

    remove vertex

--- lua/jade/filetype.lua
+++ lua/jade/filetype.lua
@@ -12,3 +12,21 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.opt_local.linebreak = true
     end,
 })
+
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = 'tex',
+    callback = function()
+        vim.keymap.set('n', '<leader>fs', function()
+            local file = vim.fn.expand('%:p')
+            local line = vim.fn.line('.')
+            local pdf = vim.fn.expand('%:p:r') .. '.pdf'
+            vim.fn.jobstart({
+                'sioyek',
+                '--reuse-window',
+                '--forward-search-file', file,
+                '--forward-search-line', tostring(line),
+                pdf
+            })
+        end, { buffer = true })
+    end,
+})
