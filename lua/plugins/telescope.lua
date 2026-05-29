4644e2943fe3272731e4580466e124697ba1a7b2
Kathy <hatesjade@gmail.com>
2026-05-29 09:53:47 +0800

    update telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,8 +1,35 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = 'v0.2.0',
-      dependencies = { 'nvim-lua/plenary.nvim' },
-      keys = {
-      { "<leader>ff", "<cmd>Telescope find_files<cr>" },
-      { "<leader>fg", "<cmd>Telescope live_grep<cr>" },
+    'nvim-telescope/telescope.nvim',
+    version = '*',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     },
-     }
+    keys = {
+        { '<leader>ff', '<cmd>Telescope find_files<cr>', desc = 'Find files' },
+        { '<leader>fg', '<cmd>Telescope live_grep<cr>',  desc = 'Live grep' },
+        { '<leader>fb', '<cmd>Telescope buffers<cr>',    desc = 'Buffers' },
+        { '<leader>fh', '<cmd>Telescope help_tags<cr>',  desc = 'Help tags' },
+    },
+    config = function(_, opts)
+        local telescope = require('telescope')
+        telescope.setup(opts)
+        telescope.load_extension('fzf')
+    end,
+    opts = {
+        defaults = {
+            vimgrep_arguments = {
+                'rg', '--color=never', '--no-heading',
+                '--with-filename', '--line-number', '--column',
+                '--smart-case', '--hidden',
+                '--glob', '!**/.git/*',
+            },
+        },
+        pickers = {
+            find_files = {
+                hidden = true,
+                find_command = { 'rg', '--files', '--hidden', '--glob', '!**/.git/*' },
+            },
+        },
+    },
+}
